b0d745efb99fab7fc2b7123e19e0e002d5c7cef2
Diego Humberto Reymy Aguilar <diegoreymy@gmail.com>
2021-05-08 11:27:55 -0300

    feat(app): creating shared folder and  user model

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-import { MaterialModule } from 'src/app/material/material.module';
+import { MaterialModule } from '../../shared/material/material.module';
 import { ReactiveFormsModule } from '@angular/forms';
 
 
